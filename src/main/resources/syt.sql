0b22dac384ab5988dd55949d4e8e86a4edb9f444
lbw <1311881356@qq.com>
2018-09-03 20:46:52 +0800

    商家后台初步整合

--- src/main/resources/syt.sql
+++ src/main/resources/syt.sql
@@ -56,10 +56,10 @@ select id,name from tb_item_cat;
 select name from tb_item_cat where id in(
   select category1_id,category2_id,category3_id from tb_goods where id=149187842867958
 );
+select id, option_name, spec_id, orders from tb_specification_option
 
 
-
-select * from tb_item_cat where parent_id =0;
+select * from tb_item_cat;
 select database();
 USE shoppingmall;
 
@@ -104,3 +104,45 @@ CREATE TABLE `tb_goods_desc` (
   `sale_service` varchar(3000) DEFAULT NULL COMMENT '售后服务',
   PRIMARY KEY (`goods_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SELECT *
+FROM tb_goods_desc ;
+
+CREATE TABLE `tb_type_template` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) DEFAULT NULL COMMENT '模板名称',
+  `spec_ids` varchar(1000) DEFAULT NULL COMMENT '关联规格',
+  `brand_ids` varchar(1000) DEFAULT NULL COMMENT '关联品牌',
+  `custom_attribute_items` varchar(2000) DEFAULT NULL COMMENT '自定义属性',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
+
+insert  into `tb_type_template`(`id`,`name`,`spec_ids`,`brand_ids`,`custom_attribute_items`) values (35,'手机','[{\"id\":27,\"text\":\"网络\"},{\"id\":32,\"text\":\"机身内存\"}]','[{\"id\":1,\"text\":\"联想\"},{\"id\":3,\"text\":\"三星\"},{\"id\":2,\"text\":\"华为\"},{\"id\":5,\"text\":\"OPPO\"},{\"id\":4,\"text\":\"小米\"},{\"id\":9,\"text\":\"苹果\"},{\"id\":8,\"text\":\"魅族\"},{\"id\":6,\"text\":\"360\"},{\"id\":10,\"text\":\"VIVO\"},{\"id\":11,\"text\":\"诺基亚\"},{\"id\":12,\"text\":\"锤子\"}]','[{\"text\":\"内存大小\"},{\"text\":\"颜色\"}]');
+insert  into `tb_type_template`(`id`,`name`,`spec_ids`,`brand_ids`,`custom_attribute_items`) values (37,'电视','[{\"id\":33,\"text\":\"电视屏幕尺寸\"}]','[{\"id\":16,\"text\":\"TCL\"},{\"id\":13,\"text\":\"长虹\"},{\"id\":14,\"text\":\"海尔\"},{\"id\":19,\"text\":\"创维\"},{\"id\":21,\"text\":\"康佳\"},{\"id\":18,\"text\":\"夏普\"},{\"id\":17,\"text\":\"海信\"},{\"id\":20,\"text\":\"东芝\"},{\"id\":15,\"text\":\"飞利浦\"},{\"id\":22,\"text\":\"LG\"}]','[]');
+SELECT * from tb_type_template;
+
+
+CREATE TABLE `tb_specification` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `spec_name` varchar(255) DEFAULT NULL COMMENT '名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+SELECT * from tb_specification;
+
+
+CREATE TABLE `tb_specification_option` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '规格项ID',
+  `option_name` varchar(200) DEFAULT NULL COMMENT '规格项名称',
+  `spec_id` bigint(30) DEFAULT NULL COMMENT '规格ID',
+  `orders` int(11) DEFAULT NULL COMMENT '排序值',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=133 DEFAULT CHARSET=utf8;
+SELECT * from tb_specification_option;
+
+CREATE TABLE `tb_brand` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL COMMENT '品牌名称',
+  `first_char` varchar(1) DEFAULT NULL COMMENT '品牌首字母',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+SELECT *
+FROM tb_brand;
